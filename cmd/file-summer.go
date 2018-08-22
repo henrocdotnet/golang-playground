560fb1405e68f6fd1d5c4634bdd718c564161cbe
Henry Rivera <henroc@henroc.net>
2018-08-22 17:08:56 -0400

    Simple File Hasher with Channels

---
+++ cmd/file-summer.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"flag"
+)
+
+var (
+	channelProcess  = make(chan hashfile)
+	channelComplete = make(chan hashfile)
+	debugMode       = false
+	directory       = "."
+	workerLimit     = 10
+)
+
+func init() {
+	flag.BoolVar(&debugMode, "debug", false, "Enable debug mode.")
+	flag.StringVar(&directory, "directory", ".", "Directory to scan.")
+	flag.IntVar(&workerLimit, "limit", 10, "Hash calculator worker limit.")
+	flag.Parse()
+}
+
+func main() {
+	fmt.Printf("WTF? %s\n", directory)
+	debugMessage("BEGIN: main")
+
+	// Test scan directory.
+	validateDirectoryOrExit(directory)
+
+	// Setup workers.
+	for i := 1; i <= workerLimit; i++ {
+		go workerProcessFile(channelProcess);
+	}
+	go workerPrintResult(channelComplete)
+
+	// Find files and send to workers..
+	filepath.Walk(directory, findFilesCallback)
+
+}
+
+func findFilesCallback(path string, info os.FileInfo, e error) error {
+	// Skip directories.
+	if info.IsDir() {
+		return nil
+	}
+
+	// Create new hashfile.
+	hf := hashfile{path: path}
+
+	// Add to process queue.
+	channelProcess <- hf
+	debugMessage("BEGIN: findFilesCallback: found %s", path)
+
+	return nil
+
+}
+
+func workerPrintResult(c chan (hashfile)) {
+	debugMessage("BEGIN: workerPrintResult")
+	for {
+		f := <- channelComplete
+		fmt.Printf("%s: %s", f.path, f.hash)
+	}
+}
+
+func workerProcessFile(c chan (hashfile)) {
+	debugMessage("BEGIN: workerProcessFile")
+	for {
+		f := <-c
+
+		bytes, err := ioutil.ReadFile(f.path)
+		if err != nil {
+			fmt.Printf("ERROR: Could not read file '%s': %s'\n", f.path, err)
+		}
+		continue
+
+		hash := md5.New()
+		f.hash = fmt.Sprintf("%x", hash.Sum(bytes))
+
+		channelComplete <- f
+	}
+}
+
+func validateDirectoryOrExit(p string) {
+	v, err := os.Stat(p)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("ERROR: directory '%s' not found.\n", p)
+		} else {
+			fmt.Printf("ERROR: error accessing directory '%s': %s'\n", p, err)
+		}
+		os.Exit(1)
+	}
+
+	if !v.IsDir() {
+		fmt.Printf("ERROR: '%s' is not a directory\n", p)
+		os.Exit(1)
+	}
+}
+
+func debugMessage(m string, v ...interface{}) {
+	if !debugMode {
+		return
+	}
+
+	log.Printf(m, v...)
+}
+
+// Types.
+// ------
+
+type hashfile struct {
+	path string
+	hash string
+}
